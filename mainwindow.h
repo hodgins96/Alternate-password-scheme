11285d249399b02b10c13aa41680e0f43ce0da4a
hodgins96 <hodgins016@gmail.com>
2019-03-24 18:30:04 -0400

    added animal names

--- mainwindow.h
+++ mainwindow.h
@@ -22,10 +22,11 @@ public:
     void enterPasswordSpawn(std::vector<QString> Paths);
     void addbuttons(std::vector<QString> path, std::vector<QString> Pass);
 private:
-    std::vector<QString> paths{"index.png"};
-    std::vector<QString> words{"index"};
-
-private:
+    std::vector<QString> paths{"Liar","Misfire","ModernTimesRock'n'Roll","MyFairyKing",
+                              "Nevermore","OgreBattle","Procession","SevenSeasofRhye","SomeDayOneDay",
+                              "SonAndDaughter","StoneColdCrazy","TheLoserInTheEnd","TheNightComesDown",
+                              "WhiteQueen","BohemianRhapsody","BrightonRock","DoingAllRight","FatherToSon",
+                              "FunnyHowLoveIs","GreatRatKing","","Jesus","KeepYourselfAlive","KillerQueen",""};
     Ui::MainWindow *ui;
 };
 
