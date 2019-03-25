88089e5001640b0c5c61336838ae8aefa07345a1
hodgins96 <hodgins016@gmail.com>
2019-03-25 19:28:35 -0400

    Added Function to assign password when called

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,7 +24,7 @@ void MainWindow::addbuttons(std::vector<QString> path)
             if((i==4 && k==0) || (i==4 && k==4)){
                 ui->gridLayout_2->spacerItem();
             }else{
-                QPixmap pix("/home/student/Documents/3008/3008-Project2/Assets/"+path[(i*5)+k]+".png");
+                QPixmap pix("/Programming/3008-Project2/Assets/"+path[(i*5)+k]+".png"); ///home/student/Documents/3008/3008-Project2
                 QIcon ButtonIcon(pix);
 
                 int w = pix.width()/6;
@@ -45,3 +45,62 @@ void MainWindow::addbuttons(std::vector<QString> path)
         }
     }
 }
+void MainWindow::pwdAssign(){ //verified that all fo the passwor strings are accounted for
+
+std::srand (time(NULL)); //makes them random
+
+int password1 = std::rand() % 24;//gets random number
+while(password1 == 20 ){ //get rid of black string in vector
+     password1 = std::rand() % 24;
+}
+int password2 = std::rand() % 24; // gets random number
+while(password2 == 20){//get rid of black string in vector
+     password2 = std::rand() % 24;
+}
+int password3 = std::rand() % 24; //gets random number
+while(password3 == 20){//get rid of black string in vector
+     password3 = std::rand() % 24;
+}
+int password4 = std::rand() % 24; //gets random number
+while(password4 == 20){//get rid of black string in vector
+     password4 = std::rand() % 24;
+}
+int password5 = std::rand() % 24; //gets random number
+while(password5 == 20){//get rid of black string in vector
+     password5 = std::rand() % 24;
+}
+
+firstPas = paths[password1]; //string of the password picture
+qDebug() << firstPas;
+
+secondPas = paths[password2]; //string of the password picture
+qDebug() << secondPas;
+
+thirdPas = paths[password3]; //string of the password picture ;)
+qDebug() << thirdPas;
+
+fourthPas = paths[password4]; //string of the password picture ;)
+qDebug() <<fourthPas;
+
+fifthPas = paths[password5]; //string of the password picture ;)
+qDebug() << fifthPas;
+
+//come back to this, make && and || statements for comparing them or do a while loop with a counter to count how many are the same
+
+//int reRoll = QString::compare(firstPas,secondPas);
+//if(reRoll == 0){
+// int beRoll = QString::compare(thirdPas,secondPas);
+//    if(beRoll == 0){
+//        password3 = std::rand() % 24;
+//        while(password3 == 20){//get rid of black string in vector
+//             password3 = std::rand() % 24;
+//        }
+//        thirdPas = paths[password3];
+//        qDebug() << thirdPas;
+//    }
+//}
+
+
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,11 @@
 #include <QIcon>
 #include <QString>
 #include <vector>
+#include <cstdlib>
+#include <typeinfo>
+#include <iostream>
+#include <QRandomGenerator>
+#include <time.h>
 
 namespace Ui {
 class MainWindow;
@@ -20,13 +25,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void addbuttons(std::vector<QString> path);
+    void pwdAssign();
 private:
-    std::vector<QString> paths{"Liar","Misfire","ModernTimesRock'n'Roll","MyFairyKing",
+    std::vector<QString> paths{
                               "Nevermore","OgreBattle","Procession","SevenSeasofRhye","SomeDayOneDay",
-                              "SonAndDaughter","StoneColdCrazy","TheLoserInTheEnd","TheNightComesDown",
-                              "WhiteQueen","BohemianRhapsody","BrightonRock","DoingAllRight","FatherToSon",
+                              "SonAndDaughter","Misfire","StoneColdCrazy","TheLoserInTheEnd","TheNightComesDown",
+                              "Liar","MyFairyKing","WhiteQueen","BohemianRhapsody","ModernTimesRock'n'Roll",
+                              "BrightonRock","DoingAllRight","FatherToSon",
                               "FunnyHowLoveIs","GreatKingRat","","Jesus","KeepYourselfAlive","KillerQueen",""};
     Ui::MainWindow *ui;
+   //used for assigning
+    QString firstPas;
+    QString secondPas;
+    QString thirdPas;
+    QString fourthPas;
+    QString fifthPas;
 };
 
 #endif // MAINWINDOW_H
